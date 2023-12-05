e7b0415636f929fe124f862992bb7c7a65bbd7d5
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2023-12-05 19:39:04 -0300

    refactor(move): Moved Supplier onModelCreate to configuration folder

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MinimalApi.Data.Configuration;
 using MinimalApi.Models;
 
 namespace MinimalApi.Data
@@ -6,27 +7,11 @@ namespace MinimalApi.Data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<Fornecedor> Fornecedor { get; set; }
+        public DbSet<Fornecedor>? Fornecedor { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Fornecedor>()
-                .HasKey(p => p.Id);
-
-            modelBuilder.Entity<Fornecedor>()
-                .Property(p => p.Name)
-                .IsRequired()
-                .HasColumnType("varchar(200)");
-
-            modelBuilder.Entity<Fornecedor>()
-                .Property(p => p.Documento)
-                .IsRequired()
-                .HasColumnType("varchar(20)");
-
-            modelBuilder.Entity<Fornecedor>()
-                .ToTable("fornecedores");
-
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppContext).Assembly);
         }
     }
 }
---
+++ Data/Configuration/FornecedorConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using MinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MinimalApi.Data.Configuration
+{
+    public class FornecedorConfiguration : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.ToTable("Fornecedores");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+            builder.Property(p => p.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(20)");
+        }
+    }
+}
